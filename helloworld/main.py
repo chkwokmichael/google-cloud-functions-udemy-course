7dc0de78e78b13546e1b15c92e1ebf2494da32af
Michael <chkwokmichael@gmail.com>
2022-06-15 17:10:33 +0100

    Handle CORS request

--- helloworld/main.py
+++ helloworld/main.py
@@ -3,6 +3,25 @@ def hello_world(request):
     This cloud function prints return "Hello World" page when there is an HTTP request.
     '''
 
+    # CORS
+    # If the funtion is called by web-broswers, we need to include a header
+    # It is called a preflight request. It's returned before the main request (returning the html to print names)
+    if request.method == 'OPTIONS':
+        headers = {
+            # Specify the specific domain, or allow everyone '*'
+            'Access-Control-Allow-Origin': '*',
+            # Specify the allowed methods to call the funciton
+            'Access-Control-Allow-Methods': 'POST',
+            # Specify the allowed headers
+            'Access-Control-Allow-Headers': 'Content-Type',
+            # Maximum time for the broswer to remember the header in seconds (1 hour = 3600s)
+            'Access-Control-Max-Age': '3600'
+        }
+
+        return '',204,headers
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
     # Create a variable `request_args`` to hold the arguments of the request `request.args`
     request_args = request.args
     # Create a variable `request_json` to catch the JSON object argument if there's any
@@ -22,4 +41,4 @@ def hello_world(request):
         name = 'World'
         lastname = 'lastname'
     
-    return f'Hello {name} {lastname}!'
+    return f'Hello {name} {lastname}!', 200, headers
